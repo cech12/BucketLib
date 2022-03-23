8ec8b3084046711ccf22eb3991a0bec97f1273ab
Christian Voß <cech12@gmail.com>
2022-03-23 20:42:07 +0100

    extend milking tests

--- src/test/java/cech12/bucketlib/BucketLibTestHelper.java
+++ src/test/java/cech12/bucketlib/BucketLibTestHelper.java
@@ -5,14 +5,36 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.gametest.framework.GameTestHelper;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Abilities;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
+import java.util.function.Function;
 
 public class BucketLibTestHelper {
+
+    public static Player makeMockCreativePlayer(GameTestHelper helper) {
+        return new Player(helper.getLevel(), BlockPos.ZERO, 0.0F, new GameProfile(UUID.randomUUID(), "test-mock-survival-player")) {
+            public boolean isSpectator() {
+                return false;
+            }
+
+            public boolean isCreative() {
+                return true;
+            }
+
+            @Override
+            @NotNull
+            public Abilities getAbilities() {
+                Abilities abilities = super.getAbilities();
+                abilities.instabuild = true;
+                return abilities;
+            }
+        };
+    }
 
     public static Player makeMockSurvivalPlayer(GameTestHelper helper) {
         return new Player(helper.getLevel(), BlockPos.ZERO, 0.0F, new GameProfile(UUID.randomUUID(), "test-mock-survival-player")) {
@@ -26,20 +48,29 @@ public class BucketLibTestHelper {
         };
     }
 
-    public static InteractionResultHolder<ItemStack> useItemStackOnBlock(GameTestHelper helper, ItemStack itemStack, BlockPos pos, boolean isCreative) {
+    public static boolean hasSpecificBucket(Player player, Function<ItemStack, Boolean> testMethod) {
+        for (ItemStack itemStack : player.getInventory().items) {
+            if (testMethod.apply(itemStack)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static PlayerInteractionResult useItemStackOnBlock(GameTestHelper helper, ItemStack itemStack, BlockPos pos, boolean isCreative) {
         BlockPos blockpos = helper.absolutePos(pos);
-        Player player = isCreative ? helper.makeMockPlayer() : makeMockSurvivalPlayer(helper);
+        Player player = isCreative ? makeMockCreativePlayer(helper) : makeMockSurvivalPlayer(helper);
         player.setItemInHand(InteractionHand.MAIN_HAND, itemStack.copy());
         //centered one block above looking down
         player.absMoveTo(blockpos.getX() + 0.5D, blockpos.getY() + 1.0D, blockpos.getZ() + 0.5D, 0F, 90F);
-        return itemStack.use(helper.getLevel(), player, InteractionHand.MAIN_HAND);
+        return new PlayerInteractionResult(player, itemStack.use(helper.getLevel(), player, InteractionHand.MAIN_HAND));
     }
 
-    public static InteractionResultHolder<ItemStack> useItemOnEntity(GameTestHelper helper, ItemStack stack, Entity entity, boolean isCreative) {
-        Player player = isCreative ? helper.makeMockPlayer() : makeMockSurvivalPlayer(helper);
+    public static PlayerInteractionResult useItemOnEntity(GameTestHelper helper, ItemStack stack, Entity entity, boolean isCreative) {
+        Player player = isCreative ? makeMockCreativePlayer(helper) : makeMockSurvivalPlayer(helper);
         player.setItemInHand(InteractionHand.MAIN_HAND, stack);
         InteractionResult interactionResult = player.interactOn(entity, InteractionHand.MAIN_HAND);
-        return new InteractionResultHolder<>(interactionResult, player.getItemInHand(InteractionHand.MAIN_HAND));
+        return new PlayerInteractionResult(player, interactionResult, player.getItemInHand(InteractionHand.MAIN_HAND));
     }
 
 }
--- src/test/java/cech12/bucketlib/EntityTests.java
+++ src/test/java/cech12/bucketlib/EntityTests.java
@@ -3,45 +3,72 @@ package cech12.bucketlib;
 import cech12.bucketlib.api.BucketLibApi;
 import cech12.bucketlib.util.BucketLibUtil;
 import net.minecraft.core.BlockPos;
-import net.minecraft.gametest.framework.GameTest;
-import net.minecraft.gametest.framework.GameTestHelper;
-import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.gametest.framework.GameTestGenerator;
+import net.minecraft.gametest.framework.TestFunction;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraftforge.gametest.GameTestHolder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @GameTestHolder(BucketLibApi.MOD_ID)
 public class EntityTests {
 
     private static final BlockPos ENTITY_POSITION = new BlockPos(1, 2, 1);
 
-    @GameTest(template = "pit")
-    public static void testMilkingACow(GameTestHelper test) {
-        ItemStack bucket = new ItemStack(BucketLibTestMod.TEST_BUCKET.get());
-        Entity entity = test.spawn(EntityType.COW, ENTITY_POSITION);
-        InteractionResultHolder<ItemStack> result = BucketLibTestHelper.useItemOnEntity(test, bucket, entity, false);
-        if (!result.getResult().consumesAction()) {
-            test.fail("Wrong InteractionResult after using an empty bucket on a cow: " + result.getResult());
+    @GameTestGenerator
+    public static List<TestFunction> generateMilkingTests() {
+        List<TestFunction> testFunctions = new ArrayList<>();
+        EntityType<?>[] entityTypes = {EntityType.COW, EntityType.GOAT};
+        int[] stackSizes = {1, 2};
+        boolean[] creativeStates = { false, true };
+        for (EntityType<?> entityType : entityTypes) {
+            for (int stackSize : stackSizes) {
+                for (boolean isCreative : creativeStates) {
+                    String entityName = Objects.requireNonNull(entityType.getRegistryName()).getPath();
+                    String testName = "test" + ((isCreative) ? "creative" : "survival") + "milkinga" + entityName + "withstack" + stackSize;
+                    testFunctions.add(new TestFunction(
+                            "defaultBatch",
+                            testName,
+                            new ResourceLocation(BucketLibApi.MOD_ID, "entitytests.pit").toString(),
+                            Rotation.NONE,
+                            100,
+                            0,
+                            true,
+                            test -> {
+                                ItemStack bucket = new ItemStack(BucketLibTestMod.TEST_BUCKET.get(), stackSize);
+                                Entity entity = test.spawn(entityType, ENTITY_POSITION);
+                                PlayerInteractionResult result = BucketLibTestHelper.useItemOnEntity(test, bucket, entity, isCreative);
+                                if (!result.getResult().consumesAction()) {
+                                    test.fail("Wrong InteractionResult after using an empty bucket on a goat: " + result.getResult());
+                                }
+                                if (BucketLibUtil.containsMilk(result.getObject()) == (isCreative || stackSize > 1)) {
+                                    test.fail("The bucket in main hand does " + (isCreative ? "" : "not ") + "contain milk after " + (isCreative ? "creative" : "survival") + " interacting with a " + entityName + " with a stack size of " + stackSize);
+                                }
+                                if (isCreative && result.getObject().getCount() != stackSize) {
+                                    test.fail("The bucket stack size " + result.getObject().getCount() + " in main hand after creative interacting with a " + entityName + " with a stack size of " + stackSize + " is not the same as before");
+                                }
+                                if (!isCreative && stackSize > 1 && result.getObject().getCount() != stackSize - 1) {
+                                    test.fail("The bucket stack size " + result.getObject().getCount() + " in main hand after survival interacting with a " + entityName + " with a stack size of " + stackSize + " is not one times lower");
+                                }
+                                if ((isCreative || stackSize > 1) && !BucketLibTestHelper.hasSpecificBucket(result.getPlayer(), BucketLibUtil::isEmpty)) {
+                                    test.fail("The player doesn't have an empty bucket after " + (isCreative ? "creative" : "survival") + " interacting with a " + entityName + " with a stack size of " + stackSize);
+                                }
+                                if (!isCreative && !BucketLibTestHelper.hasSpecificBucket(result.getPlayer(), BucketLibUtil::containsMilk)) { //in creatice mode, a vanilla milk bucket is generated
+                                    test.fail("The player doesn't have bucket filled with milk after survival interacting with a " + entityName + " with a stack size of " + stackSize);
+                                }
+                                test.succeed();
+                            }
+                    ));
+                }
+            }
         }
-        if (!BucketLibUtil.containsMilk(result.getObject())) {
-            test.fail("The bucket does not contain milk after interacting with a cow.");
-        }
-        test.succeed();
-    }
-
-    @GameTest(template = "pit")
-    public static void testMilkingAGoat(GameTestHelper test) {
-        ItemStack bucket = new ItemStack(BucketLibTestMod.TEST_BUCKET.get());
-        Entity entity = test.spawn(EntityType.GOAT, ENTITY_POSITION);
-        InteractionResultHolder<ItemStack> result = BucketLibTestHelper.useItemOnEntity(test, bucket, entity, false);
-        if (!result.getResult().consumesAction()) {
-            test.fail("Wrong InteractionResult after using an empty bucket on a goat: " + result.getResult());
-        }
-        if (!BucketLibUtil.containsMilk(result.getObject())) {
-            test.fail("The bucket does not contain milk after interacting with a goat.");
-        }
-        test.succeed();
+        return testFunctions;
     }
 
 }
