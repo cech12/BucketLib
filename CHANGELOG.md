3b16c6a32093f10029150a8074a7a8b74b6ddfdc
cech12 <cech12@gmail.com>
2025-01-28 23:45:33 +0100

    fixed: Dyeing a filled bucket duplicated it

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project adheres to [Forge Recommended Versioning](https://mcforge.readthedocs.io/en/latest/conventions/versioning/).
 
+## [1.21.3-4.2.0.4] - 2025-01-28
+### Fixed
+- Dyeing a filled bucket duplicated it
+
 ## [1.21.3-4.2.0.3] - 2025-01-28
 ### Fixed
 - Minecraft did not load correctly with a registered bucket. (Fabric)
---
+++ common/src/main/java/de/cech12/bucketlib/mixin/CraftingRecipeMixin.java
@@ -0,0 +1,34 @@
+package de.cech12.bucketlib.mixin;
+
+import de.cech12.bucketlib.api.item.UniversalBucketItem;
+import net.minecraft.core.NonNullList;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.ArmorDyeRecipe;
+import net.minecraft.world.item.crafting.CraftingInput;
+import net.minecraft.world.item.crafting.CraftingRecipe;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(CraftingRecipe.class)
+public interface CraftingRecipeMixin {
+
+    @Inject(at = @At("RETURN"), method = "getRemainingItems", cancellable = true)
+    default void getRemainingItems(CraftingInput craftingInput, CallbackInfoReturnable<NonNullList<ItemStack>> cir) {
+        if ((Object)this instanceof ArmorDyeRecipe) {
+            NonNullList<ItemStack> list = cir.getReturnValue();
+            boolean changed = false;
+            for (int i = 0; i < list.size(); i++) {
+                ItemStack stack = list.get(i);
+                if (stack.getItem() instanceof UniversalBucketItem) {
+                    list.set(i, ItemStack.EMPTY); //for bucket coloring we don't want to return the remainder
+                    changed = true;
+                }
+            }
+            if (changed) {
+                cir.setReturnValue(list);
+            }
+        }
+    }
+}
