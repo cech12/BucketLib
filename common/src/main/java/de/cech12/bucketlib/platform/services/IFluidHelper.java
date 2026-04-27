1e892ad89aea6cdff4cd90676d2a04f9303b88cc
cech12 <cech12@gmail.com>
2026-04-27 23:21:20 +0200

    fix water fluid color in Fabric

--- common/src/main/java/de/cech12/bucketlib/platform/services/IFluidHelper.java
+++ common/src/main/java/de/cech12/bucketlib/platform/services/IFluidHelper.java
@@ -14,6 +14,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.Fluids;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -27,6 +28,9 @@ public interface IFluidHelper {
 
     default int getFluidTintColor(ItemStack stack) {
         var fluid = getContainedFluid(stack);
+        if (fluid == Fluids.WATER) {
+            return -12618012;
+        }
         BlockTintSource tintSource = Minecraft.getInstance()
                 .getModelManager()
                 .getFluidStateModelSet()
