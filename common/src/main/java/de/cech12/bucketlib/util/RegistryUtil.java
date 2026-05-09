96ac6d9f9ab68f8a8d3a3af110a512dcf3ad2010
cech12 <cech12@gmail.com>
2026-05-09 09:48:02 +0200

    fixed: "bucketlib:entity" ingredient type initializing was broken

--- common/src/main/java/de/cech12/bucketlib/util/RegistryUtil.java
+++ common/src/main/java/de/cech12/bucketlib/util/RegistryUtil.java
@@ -12,13 +12,10 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.EntitySpawnReason;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.animal.Bucketable;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.MobBucketItem;
 import net.minecraft.world.item.SolidBucketItem;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.material.Fluid;
 
@@ -72,7 +69,6 @@ public class RegistryUtil {
     private static void readRegistry() {
         bucketBlocks = new ArrayList<>();
         bucketEntities = new ArrayList<>();
-        Level level = Services.PLATFORM.getCurrentLevel();
         for (Item item : Services.REGISTRY.getAllItems()) {
             if (item instanceof SolidBucketItem bucket) {
                 if (bucketBlocks.stream().noneMatch(bucketBlock -> bucketBlock.block == bucket.getBlock())) {
@@ -81,10 +77,8 @@ public class RegistryUtil {
             }
             if (item instanceof MobBucketItem bucket) {
                 EntityType<?> entityType = ((MobBucketItemAccessor) bucket).bucketlib_getEntityType();
-                if (entityType != null && level != null && entityType.create(level, EntitySpawnReason.LOAD) instanceof Bucketable) {
-                    if (bucketEntities.stream().noneMatch(bucketEntity -> bucketEntity.entityType == entityType)) {
-                        bucketEntities.add(new BucketEntity(entityType, Services.BUCKET.getFluidOfBucketItem(bucket), bucket));
-                    }
+                if (entityType != null && bucketEntities.stream().noneMatch(bucketEntity -> bucketEntity.entityType == entityType)) {
+                    bucketEntities.add(new BucketEntity(entityType, Services.BUCKET.getFluidOfBucketItem(bucket), bucket));
                 }
             }
         }
